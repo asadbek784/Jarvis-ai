6245eb0bf9d7b12ea1f5f21f91994535b9497d18
asadbek784 <asadbekkomilov545@gmail.com>
2026-04-15 22:57:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
 const CONFIG = {
-    // Google AI Studio'dan olingan bepul Gemini API kaliti
-    API_KEY: "AIzaSyBRP6weLxIlQhossU4jWWi15Th1TvV36vk", 
-    
-    // Ob-havo uchun (agar kerak bo'lsa)
-    WEATHER_API: "OB_HAVO_KALITI_IXTIYORIY"
+    API_KEY: "AIzaSyBRP6weLxIlQhossU4jWWi15Th1TvV36vk"
 };
